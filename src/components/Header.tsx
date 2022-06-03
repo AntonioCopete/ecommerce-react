04b92bda3b270fe24012e2f76f3958cd3abe28e4
AntonioCopete <antcopjim@gmail.com>
2022-06-03 19:30:09 +0200

    project setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import { BsBagCheck } from "react-icons/bs";
+const Header = () => {
+  return (
+    <header className="row">
+      <BsBagCheck size={50} />
+    </header>
+  );
+};
+
+export default Header;
